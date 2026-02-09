2779e461ae6e4eba9fe0bc6547b9074ec3c12089
Danilkulinich-shvets <danilkulinich87@gmail.com>
2026-02-09 19:27:45 +0400

    Final true

--- server.cjs
+++ server.cjs
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Все запросы перенаправляем на index.html внутри dist
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
